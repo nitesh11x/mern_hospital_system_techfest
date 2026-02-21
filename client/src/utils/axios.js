889b106b596a4878cbb6300cc209627ac5fd2cc7
Nitesh Kumar <nitesh11zx@gmail.com>
2026-02-21 19:19:08 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export const api = axios.create({
-    baseURL: import.meta.env.API_URL || 'http://localhost:1111/api',
+    baseURL: import.meta.env.VITE_APP_API_URL || 'http://localhost:1111/api',
     withCredentials: true,
     headers: { "Content-Type": "application/json" }
 })
